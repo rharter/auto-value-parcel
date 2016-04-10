a995675aa6046ccf12b5052a7da7698360cbbb7a
Brad Campbell <grandstaish@gmail.com>
2016-04-10 12:03:10 +1200

    Fix broken unit test

--- auto-value-parcel/src/test/java/android/os/Parcelable.java
+++ auto-value-parcel/src/test/java/android/os/Parcelable.java
@@ -1,6 +1,11 @@
 package android.os;
 
 public interface Parcelable {
+  public interface Creator<T> {
+    T createFromParcel(Parcel var1);
+
+    T[] newArray(int var1);
+  }
 
   int describeContents();
   void writeToParcel(Object dest, int flags);
